876b3eeebda4c9df7c7e596cc472fbfd78b6224e
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-03 17:56:26 -0500

    Challenges section

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Purmoments::Application.routes.draw do
   match '/privacy_policy' => 'main#privacy_policy', :as => 'privacy_policy'
 
   match '/challenges' => 'challenges#index', :as => 'challenges'
-
+  match '/challenge_one' => 'challenges#challenge_one', :as => 'challenge_one'
+  match '/challenge_two' => 'challenges#challenge_two', :as => 'challenge_two'
+  match '/challenge_three' => 'challenges#challenge_three', :as => 'challenge_three'
+  match '/challenge_four' => 'challenges#challenge_four', :as => 'challenge_four'
+  match '/challenge_five' => 'challenges#challenge_five', :as => 'challenge_five'
+  match '/challenge_six' => 'challenges#challenge_six', :as => 'challenge_six'
 
   mount AjoContact::Engine => '/contact'
   mount AjoRegister::Engine => '/'
