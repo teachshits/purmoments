6e4a6423c646c46dd1648ed7b2ec9e55b7cbc547
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-07 04:02:52 -0500

    add fb game

--- db/migrate/20130305041225_create_entries.rb
+++ db/migrate/20130305041225_create_entries.rb
@@ -2,6 +2,11 @@ class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
       t.integer :user_id
+      t.integer :facebook_id
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.string :location
       t.integer :challenge_id
       t.string :entry_code
       t.timestamps
